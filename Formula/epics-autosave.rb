91ef097ab44c7269be4c94d9d1474547e6df8216
Ulrik Pedersen <ukp@observatorysciences.co.uk>
2026-05-21 18:01:36 +0100

    epics-autosave: 6.0 (new formula)
    Closes #8.
    
    Signed-off-by: Ulrik Kofoed Pedersen <6115544+ulrikpedersen@users.noreply.github.com>

---
+++ Formula/epics-autosave.rb
@@ -0,0 +1,52 @@
+# typed: strict
+# frozen_string_literal: true
+
+class EpicsAutosave < Formula
+  desc "EPICS support module for automatically saving and restoring PV values"
+  homepage "https://epics-modules.github.io/autosave/"
+  url "https://github.com/epics-modules/autosave/archive/refs/tags/R6-0.tar.gz"
+  sha256 "73b00123790e813b413dd87f573f9806be1fd36a66c0014cee85a75b6f62cc7c"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+    regex(/^R(\d+(?:-\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/ulrikpedersen/epics"
+    sha256 arm64_tahoe:   "0000000000000000000000000000000000000000000000000000000000000000"
+    sha256 arm64_sequoia: "0000000000000000000000000000000000000000000000000000000000000000"
+    sha256 arm64_sonoma:  "0000000000000000000000000000000000000000000000000000000000000000"
+    sha256 sonoma:        "0000000000000000000000000000000000000000000000000000000000000000"
+    sha256 arm64_linux:   "0000000000000000000000000000000000000000000000000000000000000000"
+    sha256 x86_64_linux:  "0000000000000000000000000000000000000000000000000000000000000000"
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "epics-base"
+
+  def install
+    (buildpath/"configure/RELEASE.local").write <<~EOS
+      EPICS_BASE=#{Formula["epics-base"].opt_prefix}
+    EOS
+
+    release = buildpath/"configure/RELEASE"
+    unless release.read.include?("RELEASE.local")
+      release.open("a") { |f| f.puts "\n-include $(TOP)/configure/RELEASE.local" }
+    end
+
+    system "make", "INSTALL_LOCATION=#{prefix}"
+  end
+
+  test do
+    epics_arch = if OS.mac?
+      Hardware::CPU.arm? ? "darwin-aarch64" : "darwin-x86_64"
+    else
+      Hardware::CPU.arm? ? "linux-aarch64" : "linux-x86_64"
+    end
+
+    assert_path_exists lib/epics_arch.to_s/shared_library("libautosave")
+  end
+end
